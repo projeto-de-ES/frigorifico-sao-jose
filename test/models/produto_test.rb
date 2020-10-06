416b1937e85730d9e3447d5caf0529c5e770ebf2
victorMendes18 <victorfran18@gmail.com>
2020-10-06 00:37:28 -0300

    Criando CRUD de produto e adequando codigo para o travis e codacy

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -4,4 +4,14 @@ class ProdutoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #test "deve salvar produto com nome, categoria, quantidade em estoeu e preço" do
+  #produto = Produto.new nome: "carne", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
+  #assert produto.save
+  #end
+
+  #test "nao deve salvar produto sem nome" do
+  #produto = Produto.new nome: "", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
+  #assert_not produto.save
+  #end
 end
