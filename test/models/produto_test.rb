3e79fd7eec075621c9e097744bb3286998f6be4a
victorMendes18 <victorfran18@gmail.com>
2020-10-09 15:24:56 -0300

    teste de model de Produto

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -5,13 +5,25 @@ class ProdutoTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  #test "deve salvar produto com nome, categoria, quantidade em estoeu e preço" do
-  #produto = Produto.new nome: "carne", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
-  #assert produto.save
-  #end
+  test "deve salvar produto com nome, categoria, quantidade em estoque e preço" do
+    usuario = Usuario.new nome: "victor", login: "victor@gmailcom", senha: "11111111"
+    usuario.save
+    produto = Produto.new nome: "carne", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
+    assert produto.save
+  end
 
-  #test "nao deve salvar produto sem nome" do
-  #produto = Produto.new nome: "", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
-  #assert_not produto.save
-  #end
+  test "nao deve salvar produto sem nome" do
+    usuario = Usuario.new nome: "Thais", login: "Thais@gmailcom", senha: "22222222"
+    usuario.save
+    produto = Produto.new nome: "", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
+    assert_not produto.save
+  end
+
+  test "deve deletar um produto" do
+    usuario = Usuario.new nome: "Thais", login: "Thais@gmailcom", senha: "22222222"
+    usuario.save
+    produto = Produto.new nome: "", categoria: "boi", qtd_estoque: 12.2, preco: 10.0
+    produto.save
+    assert produto.destroy
+  end
 end
