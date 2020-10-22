e2f5478286180fc9b4a244863756632078d79407
Carlos André de Almeida Cavalcante <carlos.andre1270@gmail.com>
2020-10-22 10:59:18 -0300

    refatorando abrir caixa

--- app/models/caixa.rb
+++ app/models/caixa.rb
@@ -15,4 +15,12 @@ class Caixa < ApplicationRecord
   def getCaixaAberto (usuario_id)
     Caixa.where(usuario_id: usuario_id).where(aberto: true).where(data: Time.current).take
   end
+
+  def abrirUmCaixa (caixa, usuario_id) 
+    caixa.usuario_id = usuario_id
+    caixa.valor_total = caixa.valor_inicial
+    caixa.data = Time.current
+    caixa.aberto = true
+    caixa
+  end
 end
